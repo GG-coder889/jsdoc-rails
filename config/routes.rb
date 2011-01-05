d9ac5564c1a5058752559b14c94c7cb7a045e02c
Ryan Williams <ryan@wigg.ly>
2011-01-05 23:55:57 +1300

    Named the jsdoc root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope Jsdoc::Engine.mount_point do
-    root :to => 'jsdoc/documentation#index'
+    root :to => 'jsdoc/documentation#index', :as => 'jsdoc_root'
     match ':symbol_alias' => 'jsdoc/documentation#symbol', :symbol_alias => /[^\/]+/, :as => 'jsdoc_symbol'
     match 'source/:filename' => 'jsdoc/documentation#source', :filename => /.+/, :as => 'jsdoc_source'
     match 'raw/:filename' => 'jsdoc/documentation#raw_source', :filename => /.+/, :as => 'jsdoc_raw_source'
