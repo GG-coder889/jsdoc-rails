c922c9b8cd99bd9ca7ea55e9e3ac490aeab218b6
Ryan Williams <ryan@wigg.ly>
2011-01-05 21:46:57 +1300

    Updated class filter to show child elements of matches

--- public/javascripts/jsdoc/jsdoc.js
+++ public/javascripts/jsdoc/jsdoc.js
@@ -6,10 +6,26 @@ jQuery(document).ready(function($) {
         classTree = $('#class_tree'),
         branches = classTree.find('li');
 
+    var matched = [];
     branches.each(function(i, el) {
-      var $el = $(el);
-      if(!filter || $el.text().toLowerCase().indexOf(filter) > -1) {
+      // Already matched the parent of this node
+      if (matched.indexOf(el) > -1) {
+        return;
+      }
+      var $el = $(el),
+          txt = $.trim(el.innerText || el.textContent).toLowerCase(),
+          label = txt.split('\n')[0];
+
+
+      if (!filter || txt.indexOf(filter) > -1) {
+        if (label.indexOf(filter) > -1) {
+          $el.find('li').each(function(j, child) {
+            matched.push(child);
+            $(child).css('display', 'block');
+          });
+        }
         $el.css('display', 'block');
+        matched.push(el);
       } else {
         $el.css('display', 'none');
       }
