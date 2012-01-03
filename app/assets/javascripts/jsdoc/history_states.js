6f653328cac076d3b67a32b42cfcfe34eaf458dd
Ryan Williams <ryan@wigg.ly>
2012-01-03 18:09:28 +1300

    Better handling of history

--- app/assets/javascripts/jsdoc/history_states.js
+++ app/assets/javascripts/jsdoc/history_states.js
@@ -5,9 +5,9 @@ $(function () {
       , elemTop = element.offsetTop - 24
       , elemBottom = elemTop + $(element).height() + 48
     if (elemTop < containerTop) {
-      $(container).scrollTop(elemTop)
+        $(container).scrollTop(elemTop)
     } else if (elemBottom > containerBottom) {
-      $(container).scrollTop(elemBottom - $(container).height())
+        $(container).scrollTop(elemBottom - $(container).height())
     }
   }
 
@@ -15,9 +15,11 @@ $(function () {
     $('body').data('url', url.match(/[^#]+/)[0])
 
     var content = $('#content')
-    content.addClass('loading')
+      , wrapper = $('#wrapper')
+    wrapper.addClass('loading')
+
     $.getScript(url, function () {
-      content.removeClass('loading')
+      wrapper.removeClass('loading')
 
       // Navigate down the page to the anchor if needed
       if (url.indexOf('#') > -1) {
@@ -26,29 +28,51 @@ $(function () {
         content.scrollTop(0)
       }
     })
+
+
+    highlightTreeItem(url)
+  }
+
+  function highlightTreeItem (url) {
     $('aside li.current').removeClass('current')
 
-    var path = url.match(/http:\/\/[^\/]+([^#]*)/)[1]
-      , li = $('aside a[href="' + path + '"]').parent()
 
-    if (li[0]) {
-      li.addClass('current')
-      scrollIntoView(li.children('a')[0], 'aside nav')
+    var path = url.match(/http:\/\/[^\/]+([^#]*)/)[1]
+      , $li = $('aside a[href="' + path + '"]').parent()
+
+    if (!$li.length) {
+      $li = $('aside a[href="' + path.replace(/\/$/, '') + '"]').parent()
+    }
+
+    if ($li[0]) {
+      $li.addClass('current')
+      scrollIntoView($li.children('a')[0], 'aside nav')
     }
   }
+
 
   // Symbol URLs
   $('a[href^="' + URL_ROOT + '"]:not(.no_remote)').live('click', function (e) {
     if (this.href != location.href.match(/[^#]+/)[0]) {
-        history.pushState(null, document.title, this.href)
+        history.pushState({page: this.href}, document.title, this.href)
     }
     loadPage(this.href)
     e.preventDefault()
   })
 
-  $(window).bind('popstate', function () {
-    if ($('body').data('url') != location.href.match(/[^#]+/)[0]) {
-      loadPage(location.href)
+
+  $(window).bind('popstate', function (e) {
+    if (!$('body').data('url')) {
+        $('body').data('url', location.href.match(/[^#]+/)[0])
     }
+
+    if (e.originalEvent.state && $('body').data('url') && e.originalEvent.state.page != $('body').data('url')) {
+        loadPage(location.href)
+    } else if (!e.originalEvent.state) {
+        history.replaceState({page: $('body').data('url')}, document.title, location.href)
+    }
+    highlightTreeItem($('body').data('url'))
   })
-});
+})
+
+// vim:et:st=4:fdm=marker:fdl=0:fdc=1
