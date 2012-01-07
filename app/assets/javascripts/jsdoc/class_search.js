c71ba5e4800624af36f200bfc1b884b5e07a4bc6
Ryan Williams <ryan@wigg.ly>
2012-01-08 11:43:00 +1300

    Fix class search so it expands hidden items

--- app/assets/javascripts/jsdoc/class_search.js
+++ app/assets/javascripts/jsdoc/class_search.js
@@ -1,37 +1,44 @@
 $(function () {
-  var findClassInput = $('#find_class'); 
+  var findClassInput = $('#find_class')
   function filterClasses(filter) {
-    var filter= $.trim(filter.toLowerCase()),
-        classTree = $('#class_tree'),
-        branches = classTree.find('li');
+    var filter= $.trim(filter.toLowerCase())
+      , classTree = $('#class_tree')
+      , branches = classTree.find('li')
 
-    var matched = [];
+    var matched = []
     branches.each(function(i, el) {
       // Already matched the parent of this node
       if (matched.indexOf(el) > -1) {
-        return;
+        return
       }
-      var $el = $(el),
-          txt = $.trim(el.innerText || el.textContent).toLowerCase(),
-          label = txt.split('\n')[0];
+      var $el = $(el)
+        , txt = $.trim(el.innerText || el.textContent).toLowerCase()
+        , label = txt.split('\n')[0]
 
 
       if (!filter || txt.indexOf(filter) > -1) {
         if (label.indexOf(filter) > -1) {
           $el.find('li').each(function(j, child) {
-            matched.push(child);
-            $(child).css('display', 'block');
-          });
+            matched.push(child)
+            $(child).show()
+            $(child).parents().show()
+            $(child).parents('.collapsed + ul').each(function (k, collapsed) {
+                var $a = $(collapsed).siblings('a')
+                $(collapsed).show()
+                $a.removeClass('collapsed')
+                $a.addClass('expanded')
+            })
+          })
         }
-        $el.css('display', 'block');
-        matched.push(el);
+        $el.show()
+        matched.push(el)
       } else {
-        $el.css('display', 'none');
+        $el.hide()
       }
-    });
+    })
   }
 
   findClassInput.bind('keyup change search paste cut copy dragend', function(evt) {
-    filterClasses(evt.target.value);
-  });
-});
+    filterClasses(evt.target.value)
+  })
+})
